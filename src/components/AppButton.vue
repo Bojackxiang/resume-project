6f72329a93a617f535fd9b84e1f43b950828a174
Alex <alex@q-83.com>
2021-06-19 16:34:37 +1000

    更新了插槽的一些基本使用

---
+++ src/components/AppButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <button
+    class="app-button"
+    :class="{
+      primary: type === 'primary',
+      warning: type === 'warning',
+      error: type === 'error',
+    }"
+  >
+    <!-- text -->
+    <!-- 我们先把 子组建里面的 数据传给子组建的 slot  -->
+    <slot name="text" :btnData="btnData"></slot>
+    <!-- icon -->
+    <slot name="icon"></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    name: { type: String },
+    type: { type: String },
+  },
+  data(){
+    return {
+      btnData: {
+        text: 'deault text',
+      }
+      
+    }
+  }, 
+  created() {
+    console.log(this.type);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/config.scss";
+.primary {
+  background-color: $color_D;
+  color: $color_A;
+}
+.app-button {
+  border: 0;
+  padding: 8px;
+  border-radius: 5px;
+  font-size: 1.2rem;
+  font-weight: bold;
+
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,15 +2,27 @@
   <div>
     <PageWrapper>
       <div>what ever</div>
+      <AppButton :type="'primary'">
+        <template v-slot:text="props">
+          <!-- REVIEW 这边外面多封装了一层  -->
+          {{props.btnData.text}}
+        </template>
+        <template v-slot:icon>
+          icon
+        </template>
+      </AppButton>
     </PageWrapper>
   </div>
 </template>
 
 <script>
+import AppButton from '../components/AppButton.vue';
 import PageWrapper from "../page-wrapper/PageWrapper.vue";
+
 export default {
   components: {
     PageWrapper,
+    AppButton
   },
   created() {
     console.log;
